bcacdb279c8546736394c357a3409ec9d4a26efc
Killer_Queen <60775260+gungnir479@users.noreply.github.com>
2022-11-12 00:01:45 +0800

    visualize

---
+++ Visualize/render/src/cfg.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './cfg.css';
+
+function node(props) {
+    return (
+        <div className='node'>
+            {props.stmt}
+        </div>
+    )
+}
+
+class graph extends React.Component {
+
+}
